20e008547201e25ce49d27198c269f18120a83b6
kclopp <kclopp@gmail.com>
2015-03-14 15:44:16 -0400

    Added another aray to hold the stat names, plus a for loop to fill in the info, next will be a function of some kind.

--- heroStats.c
+++ heroStats.c
@@ -6,42 +6,36 @@
 int main(void)
 
 {
-    int hStats[6]; // aray for the stats
+    int hStats[5]; // aray for the stats
+    string statNames[5]; // aray that has names of stats 
     string hName;  // name of hero in question
+    
+    
+    statNames[0] = "str";
+    statNames[1] = "dex";
+    statNames[2] = "sta";
+    statNames[3] = "wis";
+    statNames[4] = "int";
     
     printf("What is your hero's name? ");
     hName = GetString();
     
-    printf("What is your strength ");
-    hStats[0] = GetInt();
-    
-    printf("What is your dex ");
-    hStats[1] = GetInt();
-    
-    printf("What is your stamina ");
-    hStats[2] = GetInt();
-    
-    printf("What is your charisma ");
-    hStats[3] = GetInt();
-    
-    printf("What is your wisdom ");
-    hStats[4] = GetInt();
-    
-    printf("What is your intelligence ");
-    hStats[5] = GetInt();
-    
-    
+    for(int i = 0; i < 5; i++)
+        {
+        printf("what is your %s? ", statNames[i]);
+        hStats[i] = GetInt();
+        }   
+       
     printf("\n");
     printf("Your hero's name is %s\n", hName);
     printf("*************************\n");
-    printf("*Your strength is      %i*\n", hStats[0]);
+    printf("*Your str is           %i*\n", hStats[0]);
     printf("*Your dex is           %i*\n", hStats[1]);
-    printf("*Your stamina is       %i*\n", hStats[2]);
-    printf("*Your charisma is      %i*\n", hStats[3]);
-    printf("*Your wisdom is        %i*\n", hStats[4]);
-    printf("*Your intelligence is  %i*\n", hStats[5]);
+    printf("*Your sta is           %i*\n", hStats[2]);
+    printf("*Your wis is           %i*\n", hStats[3]);
+    printf("*Your int is           %i*\n", hStats[4]);
     printf("*************************\n");
-    
+   
     
     
 }
